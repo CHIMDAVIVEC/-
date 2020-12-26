772882115ce6dfbef1f90450839373ecc9bc4bee
CHIMDAVIVEC <55950827+CHIMDAVIVEC@users.noreply.github.com>
2020-12-26 19:30:35 +0700

    Update controller.py

--- controller.py
+++ controller.py
@@ -120,4 +120,4 @@ def index():
     return render_template(template_name + '.html', iso=iso, tech=tech, econom=econom, electro=electro, graph=graph, opt=opt, maxl=maxl)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=80, debug=False)
